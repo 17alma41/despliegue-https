c6646128318aa57a170f78ba2f69720d52b2aae4
alvaro <alvarodevejer4@gmail.com>
2025-02-06 11:54:49 +0100

    add https

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 app.use(express.json());
 const {getMessages, addMessage} = require('./database.js');
+const path = require('path');
 
 const APIKEY = "123456";
 
@@ -37,6 +40,11 @@ app.post('/message', (req, res) => {
   }
 })
 
-app.listen(3000, () => {    
-    console.log('Servidor corriendo en http://localhost:3000');
-})
+const options = {
+  key: fs.readFileSync(path.join(__dirname, 'privkey.pem')),
+  cert: fs.readFileSync(path.join(__dirname, 'fullchain.pem'))
+};
+
+https.createServer(options, app).listen(3000, () => {
+  console.log('Server started on https://dev1.cyberbunny.online:3000');
+});
